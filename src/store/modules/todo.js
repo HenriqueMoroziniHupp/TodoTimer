1a1d8c3221a089d93081cbf6be601b566eefd994
HenriqueMoroziniHupp <henriquehupp@hotmail.com>
2023-05-18 23:51:32 -0300

    add news events GA4

--- src/store/modules/todo.js
+++ src/store/modules/todo.js
@@ -36,6 +36,12 @@ export default {
         commit('SET_TODOS', newTodos);
       },
       destroyTodo({ commit, getters, state }, todo) {
+
+
+        this.$gtag.event('destroy-todo', {
+          'event_category': 'todo-list',
+        });
+
         const index = state.todos.indexOf(todo);
 
         if (index !== -1) {
@@ -46,6 +52,10 @@ export default {
         }
       },
       toggleTodo({ commit, getters, state }, todo) {
+        this.$gtag.event('done-todo', {
+          'event_category': 'done-todo',
+        });
+
         const index = state.todos.indexOf(todo);
         const newTodos = getters.allTodos;
 
